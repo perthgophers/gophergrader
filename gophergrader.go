006522ba3ea0172de5ac1a1840c3131aedef9f37
Alex Dunmow <alex.dunmow@blockninja.io>
2016-07-30 12:03:19 +0800

    server

--- gophergrader.go
+++ gophergrader.go
@@ -6,12 +6,19 @@ import (
 	"net/http"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-
+func Serve(h http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		h.ServeHTTP(w, r)
+	})
 }
 
 func main() {
-
+	rtr := ninjarouter.New()
+	rtr.GET("/*", Serve(http.FileServer(http.Dir("./web/public/"))))
+	fmt.Println("Listening on port 9000...")
+	if err := rtr.Listen(":9000"); err != nil {
+		panic(err)
+	}
 }
 
 func checkErr(w http.ResponseWriter, err error) {
