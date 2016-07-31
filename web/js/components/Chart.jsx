d3bab7055980ecac496f62e10d1f35f508899022
John Nguyen <jtnguyen236@gmail.com>
2016-07-31 12:47:08 +0800

    adds styled radar chart

--- web/js/components/Chart.jsx
+++ web/js/components/Chart.jsx
@@ -12,11 +12,12 @@ export default class Chart extends Component {
         const chartData = {
             datasets: [
                 {
+                    // rgb(86,128,181)
                     data: points,
-                    backgroundColor: 'rgba(179,181,198,0.2)',
+                    backgroundColor: 'rgba(86,128,181,1)',
                     borderColor: 'rgba(179,181,198,1)',
                     pointBackgroundColor: 'rgba(179,181,198,1)',
-                    pointBorderColor: '#fff',
+                    pointBorderColor: 'rgba(86,128,181,1)',
                     pointHoverBackgroundColor: '#fff',
                     pointHoverBorderColor: 'rgba(179,181,198,1)',
                     label: 'Historical Data'
@@ -25,12 +26,38 @@ export default class Chart extends Component {
             labels: labels
         };
             // defaultFontColor: '#666'
+        const secondaryColor = "rgba(179,181,198,1)"
         const chartOptions = {
+            scale: {
+                angleLines: {
+                    color: secondaryColor,
+                    lineWidth: 2
+                },
+                gridLines: {
+                    color: secondaryColor,
+                    lineWidth: 2
+                },
+                ticks: {
+                    color: secondaryColor,
+                    fixedStepSize: 2,
+                    suggestedMax: 10,
+                    backdropColor: 'rgba(0,0,0,0)'
+                },
+                elements: {
+                    line: {
+                        lineTension: 1000
+                    }
+                },
+                pointLabels: {
+                    fontColor: secondaryColor,
+                    fontSize: 14
+                }
+            },
         }
 
 		return (
             <div className='chart'>
-                <Radar data={chartData}/>
+                <Radar data={chartData} options={chartOptions}/>
             </div>
         );
 	}
